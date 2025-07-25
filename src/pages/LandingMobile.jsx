1f6100cd380f051a4d39726f66da680d44e429ce
Emmanuel Cardenas <emmanuelcardenas@MacBook-Air-de-Emmanuel.local>
2025-07-24 18:20:36 -0600

    ✅ Reemplazo de botón con HTML nativo en LandingMobile y corrección de errores de importación

--- src/pages/LandingMobile.jsx
+++ src/pages/LandingMobile.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 import mapboxgl from "mapbox-gl";
-import { useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 mapboxgl.accessToken = "pk.eyJ1Ijoic2hpcGl0bWV4aWNvIiwiYSI6ImNtMGZzMHZ5YjA3dTEyb3B2eng1OW80bWMifQ.jw1dTx1ZKBrD8uNKzCrzjw";
 
